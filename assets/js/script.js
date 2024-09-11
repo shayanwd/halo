25153976d68a6b4d8a7459989024e8ee375f5648
Muhammad Rafay Khan <rafayhere663@gmail.com>
2024-09-11 19:36:23 +0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,20 +18,55 @@ gsap.ticker.lagSmoothing(0)
 
 
 
-Fancybox.bind("[data-fancybox]", {
-    // Your custom options
-});
+Fancybox.bind("[data-fancybox]", {});
 
 
-function homeMousemove(params) {
+function homeMousemove() {
     const hero = document.querySelector(".main-banner-sec")
     const mover = document.getElementById("move")
     if (hero && mover) {
         document.addEventListener("mousemove", (dets) => {
+            gsap.set(mover, {
+                left: `50%`,
+                top: `50%`,
+            })
             gsap.to(mover, {
                 left: `${dets.x}px`,
-                top: `${dets.y}px`
+                top: `${dets.y}px`,
+                x: "-19px",
+                yPercent: -50, duration: 1,
             })
+        })
+        hero.addEventListener("mousemove", () => {
+            gsap.to(mover, {
+                opacity: 1,
+                scale: 1,
+                duration: 0.3,
+            })
+        })
+        hero.addEventListener("mouseleave", () => {
+            gsap.to(mover, {
+                opacity: 0,
+                scale: 0.9,
+                duration: 0.3,
+            })
+        })
+    }
+
+
+
+    const bannerVidPlayer = document.querySelector(".vid-toggle")
+    const bannerVideo = document.querySelector(".banner-media video")
+
+    if (bannerVidPlayer && bannerVideo) {
+        bannerVidPlayer.addEventListener("click", () => {
+            if (bannerVideo.paused) {
+                bannerVideo.play()
+                bannerVidPlayer.classList.add("playin")
+            } else {
+                bannerVideo.pause()
+                bannerVidPlayer.classList.remove("playin")
+            }
         })
     }
 
