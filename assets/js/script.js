c0ee026481d126abbd3e19b632ffc1545528911a
Muhammad Rafay Khan <rafayhere663@gmail.com>
2024-09-13 22:57:05 +0500

    ..

--- assets/js/script.js
+++ assets/js/script.js
@@ -111,8 +111,87 @@ function contactAnimations() {
 }
 
 
+function accordions() {
+    jQuery(function ($) {
+        $(".accordions-wrap .accordion-content").css("display", "none");
 
+        $(".accordions-wrap .accordion h4").click(function () {
+            $(".accordions-wrap .accordion h4").not(this).removeClass("open");
+            $(".accordions-wrap .accordion h4").not(this).next().slideUp(300);
+            $(this).toggleClass("open");
+            $(this).next().slideToggle(300);
+        });
+    });
+    jQuery(function ($) {
+        $(".accordions-wrap-op :not(.accordion:first-child) .accordion-content").css("display", "none");
+        $(".accordions-wrap-op .accordion:first-child h4").addClass("open")
+
+        $(".accordions-wrap-op .accordion h4").click(function () {
+            $(".accordions-wrap-op .accordion h4").not(this).removeClass("open");
+            $(".accordions-wrap-op .accordion h4").not(this).next().slideUp(300);
+            $(this).toggleClass("open");
+            $(this).next().slideToggle(300);
+        });
+    });
+}
+
+
+function scrollSpy() {
+
+    const sspy = document.querySelector(".sspy-wrap")
+    if (sspy) {
+        $(document).ready(function () {
+            var sectionIds = $('.sspy-col1 ul a');
+            $(document).scroll(function () {
+                sectionIds.each(function () {
+
+                    var container = $(this).attr('href');
+                    var containerOffset = $(container).offset().top;
+                    var containerHeight = $(container).outerHeight();
+                    var containerBottom = containerOffset + containerHeight;
+                    var scrollPosition = $(document).scrollTop();
+
+                    if (scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20) {
+                        $(this).addClass('active');
+                    } else {
+                        $(this).removeClass('active');
+                    }
+                });
+            });
+        });
+    }
+
+    let tl = gsap.timeline()
+    gsap.set(".sspy-wrap", {
+        y: "200%"
+    })
+    tl.to(".sspy-wrap", {
+        scrollTrigger: {
+            trigger: ".package-sec",
+            start: "10% bottom",
+            end: "10% bottom",
+            scrub: 1,
+            // markers: true,
+        },
+        y: 0,
+        x: 0,
+    })
+    tl.to(".sspy-wrap", {
+        scrollTrigger: {
+            trigger: ".frequent-sec",
+            start: "bottom bottom",
+            end: "bottom 95%",
+            scrub: 1,
+            // markers: true,
+        },
+        immediateRender: false,
+        yPercent: 200,
+        duration: 0.1,
+    })
+}
 
 
 homeAnimations();
 contactAnimations();
+accordions();
+scrollSpy();
